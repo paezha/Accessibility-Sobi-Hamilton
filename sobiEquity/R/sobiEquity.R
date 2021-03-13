20f36b6b13180e97e1ad253f27ec8e5e9e56f046
Antonio Paez <paezha@gmail.com>
2021-03-13 18:55:16 -0500

    Major reorganization of repository to clean up files and to create (mostly data) package.

---
+++ sobiEquity/R/sobiEquity.R
@@ -0,0 +1,23 @@
+#' sobiEquity
+#'
+#' sobiEquity is a companion package for the paper "Examining spatial equity and accessibility
+#'  to a public bicycle share program using a balanced floating catchment area approach".
+#'   The package includes the data used in the analysis.
+#'
+#' @author Elise Desjardins, \email{desjae@@mcmaster.ca}
+#' @author Antonio Paez, \email{paezha@@mcmaster.ca}
+#' @docType package
+#' @name sobiEquity
+NULL
+
+#' Hamilton CMA.
+#'
+#' Boundary of the Hamilton CMA, in Ontario, Canada.
+#'
+#' @docType data
+#' @keywords datasets
+#' @keywords transportation
+#' @name hamilton_cma
+#' @usage data(hamilton_cma)
+#' @source Statistics Canada
+"hamilton_cma"
